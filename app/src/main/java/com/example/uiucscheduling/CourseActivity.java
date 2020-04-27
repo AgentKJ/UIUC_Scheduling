6c0d8e4610075cc60c73456d055207148461b269
Julian Kao <juliank3@illinois.edu>
2020-04-27 22:53:13 +0800

    4/27 22:52 Toolbar Added

--- app/src/main/java/com/example/uiucscheduling/CourseActivity.java
+++ app/src/main/java/com/example/uiucscheduling/CourseActivity.java
@@ -2,10 +2,13 @@ package com.example.uiucscheduling;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,6 +18,9 @@ public class CourseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course);
+
+        Toolbar toolBar = findViewById(R.id.courseToolbar);
+        setSupportActionBar(toolBar);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.courses);
@@ -36,5 +42,27 @@ public class CourseActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.add) {
+            Toast.makeText(getApplicationContext(), "You Clicked Add", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.delete)
+            Toast.makeText(getApplicationContext(), "You Clicked Delete", Toast.LENGTH_SHORT).show();
+        /*switch (item.getItemId()) {
+            case :
+
+            case R.id.delete:
+
+        }*/
+        return true;
     }
 }
